16afdd0b9ffecb80726de9545aeb3ed40280e1ed
Faty SOW <fatys8577@gmail.com>
2026-07-08 22:08:38 +0000

    Setup Laravel, Breeze auth, models Categorie Produit Acheteur Achat

---
+++ app/Http/Controllers/AcheteurController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Acheteur;
+use Illuminate\Http\Request;
+
+class AcheteurController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Acheteur $acheteur)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Acheteur $acheteur)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Acheteur $acheteur)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Acheteur $acheteur)
+    {
+        //
+    }
+}
